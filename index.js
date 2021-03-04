db64bf5f7bf0563b72447d0c46c5f0e9076eea52
FlaviaCohen <flaviacohen1988@gmail.com>
2021-03-04 18:45:46 -0300

    server config

---
+++ index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const path = require("path");
+const { PORT } = process.env;
+
+app.use(express.static(path.join(__dirname, "public/index.html")));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
